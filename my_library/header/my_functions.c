0e8181a2755bef67b9a11e0dccdf31c52cf5665e
NikitaTubolev <nikitann20002@gmail.com>
2025-03-18 13:32:04 +0200

    Add files via upload

---
+++ my_library/header/my_functions.c
@@ -0,0 +1,13 @@
+﻿#include "my_functions.h"
+
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int mydiv(int num1, int num2) {
+    return num1 - num2;  // пример реализации
+}
+
+int kub(int num1) {
+    return num1 * num1 * num1;
+}
---
+++ my_library/header/my_functions.h
@@ -0,0 +1,7 @@
+#ifndef MY_FUNCTIONS_H
+#define MY_FUNCTIONS_H
+
+int sum(int num1, int num2);
+int mydiv(int num1, int num2);
+int kub(int num1);
+#endif
